400b2413b510c0aba7494a1a3747838d7d5de77e
d4rk-h <herreragp334@gmail.com>
2025-09-14 19:54:46 +0100

    feat: binance-feeder/ improve binance feeder main implementation

--- binance-feeder/src/main/java/Main.java
+++ binance-feeder/src/main/java/Main.java
@@ -5,6 +5,14 @@ public class Main {
     public static void main(String[] args) {
         BinanceProvider provider = new BinanceProvider();
         ContinuousBinanceFetcher fetcher = new ContinuousBinanceFetcher(provider);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down btc fetcher...");
+            fetcher.stop();
+        }));
+        System.out.println("Starting continuous btc data collection from Binance...");
+        System.out.println("Press Ctrl+C to stop");
         fetcher.start();
+        try {Thread.currentThread().join();}
+        catch (InterruptedException e) {System.out.println("Main thread interrupted");}
     }
 }
